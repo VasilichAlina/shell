e9efc0363e46dd0a762ba03291dd9e91bc070be4
VasilichAlina <kusalina2006@gmail.com>
2025-11-06 23:11:20 +0300

    ЗАДАНИЕ 4:Добавим историю введённых команд и сохр

--- main.cpp
+++ main.cpp
@@ -1,24 +1,39 @@
 ﻿#include <iostream>
 #include <string>
-
+#include <fstream>
 
 int main() {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
+    const char* home = std::getenv("HOME");
+    std::string historyPath = std::string(home) + "/.kubsh_history";
+
     std::string input;
 
+    std::cout << "$ ";
 
     while (std::getline(std::cin, input)) {
-
         if (input == "\\q") {
-            exit;  
+            break;
+        }
+        else if (input == "history") {
+            std::ifstream historyOutput(historyPath);
+            std::string line;
+            while (std::getline(historyOutput, line)) {
+                std::cout << line << "\n";
+            }
+            historyOutput.close();
+        }
+        else if (!input.empty()) {
+            std::cout << input << "\n";
+
+            std::ofstream history(historyPath, std::ios::app);
+            history << input << "\n";
+            history.close();
         }
 
-
-        std::cout << input << "\n"; 
         std::cout << "$ ";
     }
-
 
 }
