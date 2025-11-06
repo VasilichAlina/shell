65f48c216441f627f06f89431796ba78a0e838f1
VasilichAlina <kusalina2006@gmail.com>
2025-11-06 23:24:21 +0300

    Задание 6:Добавим проверку введённой команды

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <fstream>
 
+
+
 int main() {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
@@ -18,20 +20,19 @@ int main() {
             break;
         }
         else if (input == "history") {
-
             std::ifstream historyOutput(historyPath);
             std::string line;
             while (std::getline(historyOutput, line)) {
                 std::cout << line << "\n";
             }
             historyOutput.close();
+
         }
+
         else if (input.find("echo ") == 0) {
-            
-            std::string text = input.substr(5);
 
+            std::string text = input.substr(5); 
             std::cout << text << "\n";
-
 
             std::ofstream history(historyPath, std::ios::app);
             history << input << "\n";
@@ -39,6 +40,8 @@ int main() {
         }
 
         else if (!input.empty()) {
+            std::cout << input << ": command not found\n";
+
             std::ofstream history(historyPath, std::ios::app);
             history << input << "\n";
             history.close();
