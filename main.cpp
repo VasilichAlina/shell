df62fb65b9ac30696f672b33e4163f664f2bf0d5
VasilichAlina <kusalina2006@gmail.com>
2025-11-06 22:47:43 +0300

    Add files via upload
    Задание 2: Печатает введённую строку в цикле и выходит по Ctrl+D

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
-#include <iostream>
+﻿#include <iostream>
+
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
 
-  
-   std::cout << "$ ";
+    std::cout << std::unitbuf;
 
-  std::string input;
-  std::getline(std::cin, input);
-std::cout<<input<<": command not found"<<std::endl;
+    std::cerr << std::unitbuf;
+
+
+    std::cout << "$ ";
+
+
+    std::string input;
+
+    while (std::getline(std::cin, input)) {
+        std::cout << input << "\n";
+        std::cout << "$ ";
+    }
 }
