36a0be47bd0f875fafd41d5041f438428630c3fc
VasilichAlina <kusalina2006@gmail.com>
2025-12-19 01:52:19 +0300

    7 пункт Добавить переменные окружения

--- main.cpp
+++ main.cpp
@@ -2,14 +2,12 @@
 #include <string>
 #include <fstream>
 
-
-
 int main() {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    const char* home = std::getenv("HOME");
-    std::string historyPath = std::string(home) + "/.kubsh_history";
+    std::string home = "/home/vboxuser";
+    std::string historyPath = home + "/.kubsh_history";
 
     std::string input;
 
@@ -26,19 +24,71 @@ int main() {
                 std::cout << line << "\n";
             }
             historyOutput.close();
-
         }
-
         else if (input.find("echo ") == 0) {
-
-            std::string text = input.substr(5); 
+            std::string text = input.substr(5);
             std::cout << text << "\n";
 
             std::ofstream history(historyPath, std::ios::app);
             history << input << "\n";
             history.close();
         }
+        else if (input.find("\\e ") == 0) {
+            std::string var_name = input.substr(3);
+            bool show_as_list = false;
 
+            if (var_name.find(" list") != std::string::npos) {
+                show_as_list = true;
+                var_name = var_name.substr(0, var_name.size() - 5);
+            }
+
+            std::string value;
+
+            if (var_name == "HOME") {
+                value = "/home/vboxuser";
+            }
+            else if (var_name == "PATH") {
+                value = "/usr/local/bin:/usr/bin:/bin:/usr/games";
+            }
+            else if (var_name == "USER") {
+                value = "vboxuser";
+            }
+            else if (var_name == "SHELL") {
+                value = "/bin/bash";
+            }
+            else if (var_name == "PWD") {
+                value = home;
+            }
+            else {
+                std::cout << "Переменная '" << var_name << "' не найдена\n";
+
+                std::ofstream history(historyPath, std::ios::app);
+                history << input << "\n";
+                history.close();
+
+                std::cout << "$ ";
+                continue;
+            }
+
+            if (show_as_list) {
+                size_t start = 0;
+                size_t end = value.find(':');
+
+                while (end != std::string::npos) {
+                    std::cout << value.substr(start, end - start) << "\n";
+                    start = end + 1;
+                    end = value.find(':', start);
+                }
+                std::cout << value.substr(start) << "\n";
+            }
+            else {
+                std::cout << value << "\n";
+            }
+
+            std::ofstream history(historyPath, std::ios::app);
+            history << input << "\n";
+            history.close();
+        }
         else if (!input.empty()) {
             std::cout << input << ": command not found\n";
 
@@ -49,5 +99,4 @@ int main() {
 
         std::cout << "$ ";
     }
-
 }
